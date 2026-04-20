730d0e7775a444c2da88ccd60f5e8f9f94af3d77
Albert <Albert@LAPTOP-S4EA0KB6>
2026-04-20 14:22:43 +0200

    Added link controller

---
+++ Controllers/LinkController.cs
@@ -0,0 +1,36 @@
+﻿using Labb3_API.Models;
+using Labb3_API.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labb3_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinkController : ControllerBase
+    {
+        private readonly InterestsDbContext _ctx;
+
+        public LinkController(InterestsDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet("{id}", Name = "GetLinkById")]
+        public async Task<ActionResult<GetLinkResponse>> GetLinkById(int id)
+        {
+            var link = _ctx.Links
+                .AsNoTracking()
+                .Select(GetLinkResponse.FromEntity)
+                .FirstOrDefaultAsync(l => l.Id == id);
+
+            if (link is null)
+            {
+                return NotFound($"Länken med ID: {id} hittades inte.");
+            }
+
+            return Ok(link);
+        }
+    }
+}
